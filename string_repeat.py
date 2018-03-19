6380ea30a565afdc01a8d1170f1ab1a3e1108126
neilmyers <mmyersneil@gmail.com>
2018-03-19 15:14:57 -0600

    Initial commit, added first file.

---
+++ string_repeat.py
@@ -0,0 +1,10 @@
+# Write a function called repeatStr which repeats 
+# the given string string exactly n times.
+
+
+def repeat_str(repeat, string):
+    print(string * repeat)
+        
+repeat_str(4, 'a')
+repeat_str(3, 'hello ')
+repeat_str(2, 'abc')
