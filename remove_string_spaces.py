f3a1f1fa734b85dfee0ccddf6dcfa8d1e36098ea
neilmyers <mmyersneil@gmail.com>
2018-03-20 15:12:43 -0600

    Added another new solution

---
+++ remove_string_spaces.py
@@ -0,0 +1,10 @@
+# Simple, remove the spaces from the string, then return the resultant string.
+
+def no_space(x):
+  print(x.replace(" ", ""))
+  
+no_space('8 j 8   mBliB8g  imjB8B8  jl  B')
+no_space('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')
+no_space('8aaaaa dddd r     ')
+no_space('jfBm  gk lf8hg  88lbe8 ')
+no_space('8j aam')
