a1ee4798f06cddd8468c56e639ab47904fbfe144
neilmyers <mmyersneil@gmail.com>
2018-03-22 16:48:30 -0600

    Solutions for march 22

---
+++ reversed_strings.py
@@ -0,0 +1,10 @@
+# Complete the solution so that it reverses the string value passed into it.
+
+def solution(string):
+  print(string[::-1])
+
+
+solution('world')
+solution('hello')
+solution('')
+solution('h')
